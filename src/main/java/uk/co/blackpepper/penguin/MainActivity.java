085d4a4210f85c18716c8038e8dad32b06b70298
Mark Hobson <markhobson@gmail.com>
2013-03-04 14:37:45 +0000

    Fixed warnings

--- src/main/java/uk/co/blackpepper/penguin/MainActivity.java
+++ src/main/java/uk/co/blackpepper/penguin/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends Activity {
 	private String currentServerUrl = null;
 	private WebView webView;
 	 
+	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
