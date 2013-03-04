e9575658b3a0ab567364260ad08e797ab3a47351
Mark Hobson <markhobson@gmail.com>
2013-03-04 22:16:32 +0000

    Formatted Java source

--- main/src/main/java/uk/co/blackpepper/penguin/android/MainActivity.java
+++ main/src/main/java/uk/co/blackpepper/penguin/android/MainActivity.java
@@ -1,65 +1,71 @@
 package uk.co.blackpepper.penguin.android;
 
 import uk.co.blackpepper.penguin.R;
-import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebView;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity
+{
 	private String currentServerUrl = null;
+
 	private WebView webView;
-	 
+
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-	    // Handle item selection
-	    switch (item.getItemId()) {
-	        case R.id.menu_settings:
-	        	Intent settingsIntent = new Intent(this, SettingsActivity.class);
-	            startActivity(settingsIntent);
-	            return true;
-	        default:
-	            return super.onOptionsItemSelected(item);
-	    }
-	}	
 
 	@Override
-	public void onResume() {
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		// Handle item selection
+		switch (item.getItemId())
+		{
+			case R.id.menu_settings:
+				Intent settingsIntent = new Intent(this, SettingsActivity.class);
+				startActivity(settingsIntent);
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
+
+	@Override
+	public void onResume()
+	{
 		loadWebview();
 		super.onResume();
 	}
 
-	private void loadWebview() {
+	private void loadWebview()
+	{
 		SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
 		String serverUrl = sharedPrefs.getString("penguin_server_url", "http://malcyl.github.com/penguin-android/");
-		if (!serverUrl.equals(currentServerUrl)) {
+		if (!serverUrl.equals(currentServerUrl))
+		{
 			currentServerUrl = serverUrl;
 			Log.i("loadWebView", String.format("Loading webview with url %s", currentServerUrl));
-			
+
 			webView = (WebView) findViewById(R.id.webView1);
 			webView.getSettings().setJavaScriptEnabled(true);
 			webView.loadUrl(currentServerUrl);
-			
 		}
 	}
-	
 }
--- main/src/main/java/uk/co/blackpepper/penguin/android/SettingsActivity.java
+++ main/src/main/java/uk/co/blackpepper/penguin/android/SettingsActivity.java
@@ -14,19 +14,22 @@ import java.util.List;
 
 import uk.co.blackpepper.penguin.R;
 
-public class SettingsActivity extends PreferenceActivity {
-
+public class SettingsActivity extends PreferenceActivity
+{
 	private static final boolean ALWAYS_SIMPLE_PREFS = false;
 
 	@Override
-	protected void onPostCreate(Bundle savedInstanceState) {
+	protected void onPostCreate(Bundle savedInstanceState)
+	{
 		super.onPostCreate(savedInstanceState);
 
 		setupSimplePreferencesScreen();
 	}
 
-	private void setupSimplePreferencesScreen() {
-		if (!isSimplePreferences(this)) {
+	private void setupSimplePreferencesScreen()
+	{
+		if (!isSimplePreferences(this))
+		{
 			return;
 		}
 
@@ -37,53 +40,59 @@ public class SettingsActivity extends PreferenceActivity {
 	}
 
 	@Override
-	public boolean onIsMultiPane() {
+	public boolean onIsMultiPane()
+	{
 		return isXLargeTablet(this) && !isSimplePreferences(this);
 	}
 
-	private static boolean isXLargeTablet(Context context) {
+	private static boolean isXLargeTablet(Context context)
+	{
 		return (context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_XLARGE;
 	}
 
-	private static boolean isSimplePreferences(Context context) {
-		return ALWAYS_SIMPLE_PREFS
-				|| Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB
-				|| !isXLargeTablet(context);
+	private static boolean isSimplePreferences(Context context)
+	{
+		return ALWAYS_SIMPLE_PREFS || Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB || !isXLargeTablet(context);
 	}
 
 	@Override
 	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	public void onBuildHeaders(List<Header> target) {
-		if (!isSimplePreferences(this)) {
+	public void onBuildHeaders(List<Header> target)
+	{
+		if (!isSimplePreferences(this))
+		{
 			loadHeadersFromResource(R.xml.pref_headers, target);
 		}
 	}
 
-	private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
-		@Override
-		public boolean onPreferenceChange(Preference preference, Object value) {
-			String stringValue = value.toString();
+	private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener =
+		new Preference.OnPreferenceChangeListener()
+		{
+			@Override
+			public boolean onPreferenceChange(Preference preference, Object value)
+			{
+				String stringValue = value.toString();
 
-			preference.setSummary(stringValue);
-			
-			return true;
-		}
-	};
+				preference.setSummary(stringValue);
 
-	private static void bindPreferenceSummaryToValue(Preference preference) {
+				return true;
+			}
+		};
+
+	private static void bindPreferenceSummaryToValue(Preference preference)
+	{
 		preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
 
-		sBindPreferenceSummaryToValueListener.onPreferenceChange(
-				preference,
-				PreferenceManager.getDefaultSharedPreferences(
-						preference.getContext()).getString(preference.getKey(),
-						""));
+		sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
+			PreferenceManager.getDefaultSharedPreferences(preference.getContext()).getString(preference.getKey(), ""));
 	}
 
 	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	public static class GeneralPreferenceFragment extends PreferenceFragment {
+	public static class GeneralPreferenceFragment extends PreferenceFragment
+	{
 		@Override
-		public void onCreate(Bundle savedInstanceState) {
+		public void onCreate(Bundle savedInstanceState)
+		{
 			super.onCreate(savedInstanceState);
 			addPreferencesFromResource(R.xml.pref_general);
 
