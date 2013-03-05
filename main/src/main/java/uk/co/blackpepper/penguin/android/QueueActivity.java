4add7aff134af8778d9c9a945961ad470ce38154
Mark Hobson <markhobson@gmail.com>
2013-03-05 16:12:16 +0000

    Added UI for renaming a queue, server call not done yet

--- main/src/main/java/uk/co/blackpepper/penguin/android/QueueActivity.java
+++ main/src/main/java/uk/co/blackpepper/penguin/android/QueueActivity.java
@@ -13,6 +13,8 @@ import android.content.AsyncTaskLoader;
 import android.content.Loader;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class QueueActivity extends ListActivity implements LoaderManager.LoaderCallbacks<Queue>
 {
@@ -26,6 +28,8 @@ public class QueueActivity extends ListActivity implements LoaderManager.LoaderC
 	
 	private String queueId;
 	
+	private Queue queue;
+	
 	// Activity methods -------------------------------------------------------
 	
 	@Override
@@ -38,6 +42,28 @@ public class QueueActivity extends ListActivity implements LoaderManager.LoaderC
 		queueId = getIntent().getExtras().getString("id");
 		
 		getLoaderManager().initLoader(0, null, this);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		getMenuInflater().inflate(R.menu.activity_queue, menu);
+		
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		switch (item.getItemId())
+		{
+			case R.id.menu_rename:
+				showQueueEditDialog(queue);
+				return true;
+				
+			default:
+				return super.onOptionsItemSelected(item);
+		}
 	}
 	
 	// LoaderCallbacks methods ------------------------------------------------
@@ -72,6 +98,8 @@ public class QueueActivity extends ListActivity implements LoaderManager.LoaderC
 	@Override
 	public void onLoadFinished(Loader<Queue> loader, Queue queue)
 	{
+		this.queue = queue;
+		
 		setTitle(queue.getName());
 	}
 
@@ -79,5 +107,19 @@ public class QueueActivity extends ListActivity implements LoaderManager.LoaderC
 	public void onLoaderReset(Loader<Queue> loader)
 	{
 		setTitle(R.string.title_activity_queue);
+	}
+	
+	// private methods --------------------------------------------------------
+	
+	private void showQueueEditDialog(Queue queue)
+	{
+		QueueEditDialogFragment fragment = new QueueEditDialogFragment();
+		
+		Bundle arguments = new Bundle();
+		arguments.putString(QueueEditDialogFragment.ID, queue.getId());
+		arguments.putString(QueueEditDialogFragment.NAME, queue.getName());
+		fragment.setArguments(arguments);
+		
+		fragment.show(getFragmentManager(), "queueEdit");
 	}
 }
