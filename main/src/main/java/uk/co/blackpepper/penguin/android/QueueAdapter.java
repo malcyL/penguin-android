ac22f9f44bea2431f163619426ef481dbce0e01b
MalcyL <malcolm@landonsonline.me.uk>
2013-03-18 21:39:16 +0000

    Queue list uses list_row layout, displaying the number of pending stories.

--- main/src/main/java/uk/co/blackpepper/penguin/android/QueueAdapter.java
+++ main/src/main/java/uk/co/blackpepper/penguin/android/QueueAdapter.java
@@ -2,20 +2,28 @@ package uk.co.blackpepper.penguin.android;
 
 import java.util.List;
 
+import uk.co.blackpepper.penguin.R;
 import uk.co.blackpepper.penguin.client.Queue;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class QueueAdapter extends ArrayAdapter<Queue>
 {
+	// fields -----------------------------------------------------------------
+    
+	private final Context context;
+
 	// constructors -----------------------------------------------------------
 	
 	public QueueAdapter(Context context, int textViewResourceId)
 	{
 		super(context, textViewResourceId);
+		this.context = context;
 	}
 	
 	// Adapter methods --------------------------------------------------------
@@ -23,11 +31,23 @@ public class QueueAdapter extends ArrayAdapter<Queue>
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent)
 	{
-		View view = super.getView(position, convertView, parent);
+		LayoutInflater inflater = (LayoutInflater) context
+			.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View view = inflater.inflate(R.layout.list_row, parent, false);
 		
-		Queue item = getItem(position);
-		((TextView) view).setText(item.getName());
+		Queue queue = getItem(position);
 
+		TextView authorTextView = (TextView) view.findViewById(R.id.title);
+		authorTextView.setText(queue.getName());
+
+		TextView subTitleTextView = (TextView) view.findViewById(R.id.sub_title);
+		subTitleTextView.setText(
+			context.getResources().getString(R.string.queue_list_item_pending) + " " + 
+			queue.getPendingCount());
+
+		ImageView imageView = (ImageView) view.findViewById(R.id.thumbnail);
+		imageView.setImageResource(R.drawable.ic_launcher);
+		
 		return view;
 	}
 	
